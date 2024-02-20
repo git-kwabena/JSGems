c4aacf777a9519fd5a55665aa00032971b846223
Kwabena Afoakwah <josephafoakwah@gmail.com>
2024-02-20 16:49:57 +0000

    Initial Commit

---
+++ inheritance.js
@@ -0,0 +1,31 @@
+class Person {
+    constructor (_name, _age){
+        this.name = _name
+        this.age = _age
+    }
+
+    description (){
+        return console.log(`This person's name is ${this.name} and he is ${this.age} years old`)
+    }
+}
+
+class Programmer extends Person{
+    // extends keywords brings all properties of Person class available to Programmer
+    constructor(_name , _age ,_title){
+        //super keyword makes properties of Person constructor available to Programmer
+        super(_name, _age)
+
+        this.title = _title
+    }
+
+
+}
+
+
+var person1 = new Person("Kwabena", 20)
+
+var person2 = new Programmer("Joseph", 30, "Software Engineer")
+
+person1.description()
+
+person2.description()
